c97a7e07ef919e7857b3e0ef15217921c5b3f125
utsbgd <ljubisa.cvetic@ut-skola.edu.rs>
2026-09-12 21:03:37 +0200

    Change PDF iframe height to 500px
    Updated iframe height for PDF display from 700px to 500px.

--- lessons/common-scripts.js
+++ lessons/common-scripts.js
@@ -17,7 +17,7 @@ function pdfLekcija(naslov, pdfPath) {
     <section>
       <h2>${naslov}</h2>
       <!-- PDF – prikaz na računaru -->
-      <iframe src="${pdfPath}" width="90%" height="700px"></iframe>
+      <iframe src="${pdfPath}" width="90%" height="500px"></iframe>
       <!-- Link za telefone i browsere koji ne prikazuju PDF -->
       <div id="pdfMobileLink">
         <a href="${pdfPath}" target="_blank" class="pdf-link">${naslov}</a>
