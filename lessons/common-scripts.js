ed57ff0ae8e53a53f9040d4d9008c7a4edb2fa94
utsbgd <ljubisa.cvetic@ut-skola.edu.rs>
2026-09-12 19:28:17 +0200

    Add pdfLekcija function for PDF lesson display
    Added a new function to generate PDF lesson sections.

--- lessons/common-scripts.js
+++ lessons/common-scripts.js
@@ -1,4 +1,3 @@
-<script>
 function slikaLekcija(naslov, imgPath, altText = "") {
   return `
     <section>
@@ -12,4 +11,18 @@ function slikaLekcija(naslov, imgPath, altText = "") {
     </section>
   `;
 }
-</script>
+
+function pdfLekcija(naslov, pdfPath) {
+  return `
+    <section>
+      <h2>${naslov}</h2>
+      <!-- PDF – prikaz na računaru -->
+      <iframe src="${pdfPath}" width="90%" height="700px"></iframe>
+      <!-- Link za telefone i browsere koji ne prikazuju PDF -->
+      <div id="pdfMobileLink">
+        <a href="${pdfPath}" target="_blank" class="pdf-link">${naslov}</a>
+      </div>
+    </section>
+  `;
+}
+
